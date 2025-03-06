83614551797e79313508e78b5f96ca1932e0da35
Rafael Gasperi <rafgasperi@yahoo.com>
2025-03-06 15:15:13 -0500

    client model with factory trait

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends BaseModel
 {
+
+    use HasFactory;
+
     public function patients(){
         return $this->belongsToMany(Patient::class,'patient_clients');
     }
