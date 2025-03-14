98d01b4f2401541210c150a40323979f0c1c8599
Rafael Gasperi <rafgasperi@yahoo.com>
2025-03-14 08:53:34 -0500

    component tipo select2 en livewire

--- app/Livewire/SearchDropdown.php
+++ app/Livewire/SearchDropdown.php
@@ -2,6 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Models\Consultation;
+use App\Models\ConsultationData;
+use App\Models\ConsultationField;
 use Livewire\Component;
 use Illuminate\Support\Facades\Http;
 
@@ -11,9 +14,17 @@ class SearchDropdown extends Component
     public $results = [];
     public $selectedOption = null;
     public $path;
+    public $consultation_field_id;
+    public $consultation_field;
+    public $consultation;
+    public $is_patient = false;
 
-    public function mount($path){
+    public function mount($path,$consultation_field_id=null,$consultation_id=null,$is_patient=false){
         $this->path = url($path);
+        $this->consultation_field_id = $consultation_field_id;
+        $this->consultation = Consultation::find($consultation_id);
+        $this->consultation_field = ConsultationField::find($consultation_field_id);
+        $this->is_patient=$is_patient;
     }
 
     public function updatedQuery()
