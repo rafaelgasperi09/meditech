269ad99097e238f99404b9b9996cb5aa5f02a89f
Rafael Gasperi <rafgasperi@yahoo.com>
2025-04-28 08:19:18 -0500

    subiendo assets de plantilla

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,94 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<?php $page = 'login'; ?>
+@extends('layout.mainlayout')
+@section('content')
+    <div class="container-fluid px-0">
+        <div class="row">
+            <!-- Login logo -->
+            <div class="col-lg-6 login-wrap">
+                <div class="login-sec">
+                    <div class="log-img">
+                        <img class="img-fluid" src="{{ URL::asset('/assets/img/login-02.png') }}" alt="Logo">
+                    </div>
+                </div>
+            </div>
+            <!-- /Login logo -->
+            <!-- Login Content -->
+            <div class="col-lg-6 login-wrap-bg">
+                <div class="login-wrapper">
+                    <div class="loginbox">
+                        <div class="login-right">
+                            <div class="login-right-wrap">
+                                <div class="account-logo">
+                                    <a href="{{ url('/') }}"><img src="{{ URL::asset('/assets/img/login-logo.png') }}"
+                                                                  alt=""></a>
+                                </div>
+                                <h2>Login</h2>
+                                <!-- Form -->
+                                <form action="{{ route('login') }}" method="POST">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label>Email <span class="login-danger">*</span></label>
+                                        <input class="form-control" type="text" id="email" value="" name="email">
+                                        <div class="text-danger pt-2">
+                                            @error('0')
+                                            {{ $message }}
+                                            @enderror
+                                            @error('email')
+                                            {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password <span class="login-danger">*</span></label>
+                                        <input class="form-control pass-input" type="password" id="password" name="password" value="">
+                                        <span class="profile-views feather-eye-off toggle-password"></span>
+                                        <div class="text-danger pt-2">
+                                            @error('0')
+                                            {{ $message }}
+                                            @enderror
+                                            @error('password')
+                                            {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="forgotpass">
+                                        <div class="remember-me">
+                                            <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
+                                                <input type="checkbox" name="radio">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                        </div>
+                                        <a href="{{ url('forgot-password') }}">Forgot Password?</a>
+                                    </div>
+                                    <div class="form-group login-btn">
+                                        <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                    </div>
+                                </form>
+                                <!-- /Form -->
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                <div class="next-sign">
+                                    <p class="account-subtitle">Need an account? <a href="{{ url('register') }}">Sign Up</a>
+                                    </p>
+                                    <!-- Social Login -->
+                                    <div class="social-login">
+                                        <a href="javascript:;"><img
+                                                src="{{ URL::asset('/assets/img/icons/login-icon-01.svg') }}"
+                                                alt=""></a>
+                                        <a href="javascript:;"><img
+                                                src="{{ URL::asset('/assets/img/icons/login-icon-02.svg') }}"
+                                                alt=""></a>
+                                        <a href="javascript:;"><img
+                                                src="{{ URL::asset('/assets/img/icons/login-icon-03.svg') }}"
+                                                alt=""></a>
+                                    </div>
+                                    <!-- /Social Login -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Login Content -->
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
