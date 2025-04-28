24ce4d9fcba21c8313ff32d236d78336c1201b73
Rafael Gasperi <rafgasperi@yahoo.com>
2025-04-28 08:20:39 -0500

    arreglando las etiquetas del locale

---
+++ resources/lang/es/generic.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'list'=>'Lista',
+    'create'=>'Crear',
+    'edit'=>'Editar',
+    'update'=>'Actualizar',
+    'save'=>'Guardar',
+    'cancel'=>'Cancelar',
+    'back'=>'Atras',
+    'detail'=>'Detalle',
+];
